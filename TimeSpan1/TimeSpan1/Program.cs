692e1e78f15b7f77f0df00d813bc89592e5d6df3
Thaismara1 <118394328+Thaismara1@users.noreply.github.com>
2023-11-09 20:40:42 +0000

    Add files via upload

---
+++ TimeSpan1/TimeSpan1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var stopwatch = new Stopwatch ();
+        stopwatch.Start = 10;
+        stopwatch.Stop = 20;
+
+        Console.WriteLine(stopwatch.TimeSpend);
+
+    }
+}
---
+++ TimeSpan1/TimeSpan1/Stopwatch.cs
@@ -0,0 +1,24 @@
+﻿using System.Drawing;
+
+public class Stopwatch
+{
+    public int Start;
+    public int Stop;
+
+    public void Point(int start, int stop)
+    {
+        this.Start = start;
+        this.Stop = stop;
+    }
+    public int TimeSpend
+    {
+        get
+        {
+            var timeSpan = Stop - Start;
+            var time = timeSpan;
+            return time; 
+
+        }
+
+    }
+}
